de4e47f9b70842a97b24c933d284fbd5a80bffd7
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2019-10-05 17:26:58 -0400

    Bump version

--- mygcc-plus.user.js
+++ mygcc-plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MyGCC plus
 // @namespace    https://github.com/jakethurman/mygcc-plus
-// @version      1.44.3
+// @version      1.44.4
 // @description  mygcc-plus
 // @downloadURL  https://github.com/jakethurman/mygcc-plus/raw/master/mygcc-plus.user.js
 // @author       Jake Thurman and Ian Spryn
